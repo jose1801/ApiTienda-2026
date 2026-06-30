5ab8a3cc9665625b628946b9ab8b572dbeaf5992
jose1801 <jdaniel.salinas03@gmail.com>
2026-06-30 14:25:54 -0500

    Feat: Agregar ruta GET pedidos al enrutador

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-// 🌟 CORREGIDO: Cambiado '../controllers/' por '../controladores/'
-import { guardarPedido } from '../controladores/pedidosCtrl.js'; 
+// 🌟 Agregamos 'obtenerPedidos' a la importación del controlador
+import { guardarPedido, obtenerPedidos } from '../controladores/pedidosCtrl.js'; 
 
 const router = Router();
+
+// 🚀 NUEVA RUTA: Para listar los pedidos con sus detalles
+router.get('/pedidos', obtenerPedidos);
 
 // Ruta para procesar la factura y el pedido
 router.post('/pedidos', guardarPedido);
